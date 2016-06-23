63a82ad21f4e77778ce79a1fe5a634b939066b0d
Karlo Smid <karlo.smid@gmail.com>
2016-06-23 20:56:12 +0200

    thanks to forum I learned: use range, use nicer output printing, no warning

--- first_steps/functions-3.exs
+++ first_steps/functions-3.exs
@@ -1,16 +1,10 @@
 fizz_buzz = fn
-  0,0,_ -> IO.puts "FizzBuzz"
-  0,_,_ -> IO.puts "Fizz"
-  _,0,_ -> IO.puts "Buzz"
-  a,b,c -> IO.puts c
+  0,0,_ -> "FizzBuzz"
+  0,_,_ -> "Fizz"
+  _,0,_ -> "Buzz"
+  _,_,c -> c
 end
 enter_fizz_buzz = fn
   n -> fizz_buzz.(rem(n,3),rem(n,5),n)
 end
-enter_fizz_buzz.(10)
-enter_fizz_buzz.(11)
-enter_fizz_buzz.(12)
-enter_fizz_buzz.(13)
-enter_fizz_buzz.(14)
-enter_fizz_buzz.(15)
-enter_fizz_buzz.(16)
+IO.inspect Enum.map(10..16,enter_fizz_buzz)
