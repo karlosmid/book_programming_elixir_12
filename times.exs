1a393a2b47e1031a84025b94ef40f6fd1b12045e
Karlo Smid <karlo.smid@gmail.com>
2016-06-25 15:29:51 +0200

    function pipe, use elixir phylosophy!

--- times.exs
+++ times.exs
@@ -4,5 +4,5 @@ defmodule Times do
   end
   def double_one_line(n), do: n * 2
   def triple(n), do: n * 3
-  def quadruple(n), do: 2 * double_one_line n
+  def quadruple(n), do: double_one_line double_one_line n
 end
