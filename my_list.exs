25d3333bb1a069eea4938481b71e7771c1bde4d3
Karlo Smid <karlo.smid@gmail.com>
2016-06-29 21:41:08 +0200

    various recursion tricks on list

--- my_list.exs
+++ my_list.exs
@@ -1,4 +1,15 @@
 defmodule MyList do
   def len([]), do: 0
   def len([_|tail]), do: 1 + len(tail)
+  def square([]), do: []
+  def square([head|tail]), do: [head*head|square(tail)]
+  def add([]), do: []
+  def add([head|tail]), do: [head+1|add(tail)]
+  def map([],_func), do: []
+  def map([head|tail], func), do: [func.(head) | map(tail,func)]
+  def sum([],total), do: total
+  def sum([head|tail], total) do
+    IO.puts total
+    sum(tail, head+total)
+  end
 end
