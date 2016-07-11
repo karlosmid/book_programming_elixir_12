07c3c19475a018a697cb6e656156ce0baee39f23
Karlo Smid <karlo.smid@gmail.com>
2016-07-11 22:23:46 +0200

    split with alternative cases

--- ListAndRecursion-5.exs
+++ ListAndRecursion-5.exs
@@ -1,4 +1,14 @@
 defmodule ListAndRecursion do
   def all?([head|tail],func), do: if func.(head) == false, do: false, else: all?(tail,func)
   def all?([],_), do: true
+  def each([head|tail],func), do: [func.(head)|each(tail,func)]
+  def each([],_), do: []
+  def filter([head|tail],func), do: if func.(head) == true, do: [head|filter(tail,func)], else: filter(tail,func)
+  def filter([],_), do: []
+  def split([head|tail],count), do: _split([head|tail],count,[])
+  defp _split(list,count,left) when (count > 0 and count > length(list)), do: {list,left}
+  defp _split(list,count,left) when (count < 0 and (count * -1) > length(list)), do: {left,list}
+  defp _split(list,count,left) when count < 0, do: _split(list, length(list) + count,left)
+  defp _split([head|tail],count,left) when count > 0, do: _split(tail, count - 1,left++[head])
+  defp _split(list,0,left), do: {left,list}
 end
