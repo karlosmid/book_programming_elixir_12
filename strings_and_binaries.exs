ef7913e1c64dc54f0f7b81a8b658b6d7217f2b1b
Karlo Smid <karlo.smid@gmail.com>
2016-07-26 21:59:02 +0200

    my first elixir parser :)

--- strings_and_binaries.exs
+++ strings_and_binaries.exs
@@ -6,4 +6,19 @@ defmodule StringsAndBinaries do
   def prepare_words(words), do: words |> List.to_string |> String.replace(" ","") |> String.upcase |> String.to_charlist
   defp _anagram([]), do: true
   defp _anagram(_), do: false
+  def calculate(expression) do
+    case Regex.named_captures(~r/(?<first>[123456789]\d*)\s*(?<operand>[\+-\\*\/])\s*0*(?<second>[123456789]\d*)/, expression) do
+      %{"first" => a, "operand" => "+", "second" => b} ->
+        elem(Integer.parse(a),0) + elem(Integer.parse(b),0)
+      %{"first" => a, "operand" => "-", "second" => b} ->
+        elem(Integer.parse(a),0) - elem(Integer.parse(b),0)
+      %{"first" => a, "operand" => "*", "second" => b} ->
+        elem(Integer.parse(a),0) * elem(Integer.parse(b),0)
+      %{"first" => a, "operand" => "/", "second" => b} ->
+        elem(Integer.parse(a),0) / elem(Integer.parse(b),0)
+      %{"first" => _, "operand" => non_support, "second" => _} ->
+        raise "Not supported opperand '#{non_support}'"
+      _ -> raise "Not parsable input"
+    end
+  end
 end
