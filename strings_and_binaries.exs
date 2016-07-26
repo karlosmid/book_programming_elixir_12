19abe783c0e7da9a741e1627ccb3cabf91571d48
Karlo Smid <karlo.smid@gmail.com>
2016-07-26 17:23:58 +0200

    first version

--- strings_and_binaries.exs
+++ strings_and_binaries.exs
@@ -2,4 +2,7 @@ defmodule StringsAndBinaries do
   def contains_all_ascii_printables([h|_]) when h < ?\s or h > ?~, do: false
   def contains_all_ascii_printables([_|t]), do: contains_all_ascii_printables(t)
   def contains_all_ascii_printables([]), do: true
+  def anagram?(word1,word2), do: _anagram(:string.to_lower(word1) -- :string.to_lower(word2))
+  defp _anagram([]), do: true
+  defp _anagram(_), do: false
 end
