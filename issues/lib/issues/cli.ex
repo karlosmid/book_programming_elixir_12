909b26bfe831056b3f91089f0352a84c920f38b9
Karlo Smid <karlo.smid@gmail.com>
2016-08-14 10:00:00 +0200

    refactoring table formatter in separate module

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -39,48 +39,7 @@ defmodule Issues.CLI do
    |> convert_to_list_of_maps
    |> sort_into_ascending_order
    |> Enum.take(count)
-   |> table_print(["number", "created_at", "title"])
- end
- def table_print issues,columns do
-   header_column_lengths = column_widths(issues,columns)
-   IO.puts create_header(columns,header_column_lengths)
-   IO.puts create_separator(columns,header_column_lengths)
-   issues
-   |> Enum.map(fn(x) -> IO.puts(create_body(columns,header_column_lengths,x)) end)
- end
- def get_longer column_info do
-   case {String.length(elem(column_info,0)),elem(column_info,1)} do
-     {_,0} -> 0
-     {x,y} when x > y -> x
-     {_,y} -> y
-   end
- end
- def create_separator(columns,header_column_lengths) do
-   columns
-   |> Enum.zip(header_column_lengths)
-   |> Enum.map(fn(x) -> "+#{String.duplicate("-",get_longer(x))}" end)
-   |> Enum.join
- end
- def create_body(columns,header_column_lengths,row) do
-   columns
-   |> Enum.zip(header_column_lengths)
-   |> Enum.map(fn(x) -> "|#{String.pad_trailing(to_string(row[elem(x,0)]),get_longer(x))}" end)
-   |> Enum.join
- end
- def create_header(columns,header_column_lengths) do
-   columns
-   |> Enum.zip(header_column_lengths)
-   |> Enum.map(fn(x) -> "|#{String.pad_trailing(elem(x,0),get_longer(x))}" end)
-   |> Enum.join
- end
- def column_widths issues,columns do
-   columns
-   |> Enum.map(fn(n) -> String.length(column_max_width(issues,n)) end)
- end
- def column_max_width list,name do
-   list
-   |> Enum.map(fn(x) -> to_string(x[name]) end)
-   |> Enum.max_by(&String.length/1)
+   |> Issues.TableFormatter.table_print(["number", "created_at", "title"])
  end
  def sort_into_ascending_order(list_of_issues) do
    Enum.sort list_of_issues, fn i1,i2 -> i1["created_at"] <= i2["created_at"] end
