337d3a1abd7c1ea68304a3af5cbd719ff1b87910
Karlo Smid <karlo.smid@gmail.com>
2016-09-19 22:33:34 +0200

    clients round robin chain

--- ticker.ex
+++ ticker.ex
@@ -3,52 +3,48 @@ defmodule Ticker do
   @name :ticker
 
   def start do
-    pid = spawn(__MODULE__, :generator, [[]])
+    pid = spawn(__MODULE__, :generator, [nil])
     :global.register_name(@name,pid)
   end
   def register(client_pid) do
     send :global.whereis_name(@name), {:register,client_pid}
   end
   
-  def do_round_robin_tick(list,len) when len > 0, do: send_and_round(list)
-  def do_round_robin_tick(list,len) when len == 0, do: []
-  
-  
-  def send_and_round(list) do
-    send Enum.at(list,0), {:tick}
-    round_robin(list)
-  end
-
-  def round_robin list do
-    [head|tail] = list
-    tail ++ [head]
-  end
-
-  def generator(clients) do
+  def generator(next) do
     receive do
       {:register, pid} ->
         IO.puts "registering #{inspect pid}"
-        generator([pid|clients])
-    after
-      @interval ->
-        IO.puts "tick"
-        clients = do_round_robin_tick(clients,length(clients))
-        IO.puts "#{inspect clients}"
-        generator(clients)
+        case next do
+          nil -> 
+            send pid, {:next,self}
+            send pid, {:tick}
+          _ -> send pid, {:next,next}
+        end
+        generator(pid)
+      {:tick} ->
+        :timer.sleep @interval
+        send next, {:tick}
+      generator(next)
     end
   end
 end
 
 defmodule Client do
+  @interval 2000
   def start do
-    pid = spawn(__MODULE__,:receiver,[])
+    pid = spawn(__MODULE__,:receiver,[nil])
     Ticker.register(pid)
   end
-  def receiver do
+  def receiver(next) do
     receive do
       {:tick} ->
         IO.puts "Tock in client"
-        receiver
+        :timer.sleep(@interval)
+        send next, {:tick}
+        receiver(next)
+      {:next,next} ->
+        IO.puts "next is #{inspect next}"
+        receiver(next)
     end
   end
 end
