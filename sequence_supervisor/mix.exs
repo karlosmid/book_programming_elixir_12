2a7618b9204b1895856e8710a12ab322b4689555
Karlo Smid <karlo.smid@gmail.com>
2016-10-19 21:26:10 +0200

    otp application

--- sequence_supervisor/mix.exs
+++ sequence_supervisor/mix.exs
@@ -14,8 +14,11 @@ defmodule SequenceSupervisor.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
-     mod: {Sequence, []}]
+    [
+     mod: {Sequence, []},
+     env: [initial_number: 456],
+     registered: [ Sequence.Server ]
+   ]
   end
 
   # Dependencies can be Hex packages:
